c01fb83d2b9d74d9fd4d5cd31874a83c69f038e4
DanielHalpin02 <G00436639@atu.ie>
2026-07-28 14:53:51 +0100

    initial MAUI template

---
+++ GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "RepeatMusicPlayer")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "RepeatMusicPlayer.Pages")]
