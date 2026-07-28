50b0abe4aeba16fea589940231278eee65b35ebe
DanielHalpin02 <G00436639@atu.ie>
2026-07-28 19:49:01 +0100

    tapping a song now takes you to now playing

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -9,4 +9,15 @@ public partial class MainPage : ContentPage
         InitializeComponent();
         BindingContext = new LibraryViewModel();
     }
+
+    private async void OnSongSelected(object sender, SelectionChangedEventArgs e)
+    {
+        if (e.CurrentSelection.FirstOrDefault() is Models.Song selectedSong)
+        {
+            await Shell.Current.GoToAsync(nameof(NowPlayingPage), new Dictionary<string, object>
+            {
+                { "Song", selectedSong }
+            });
+        }
+    }
 }
