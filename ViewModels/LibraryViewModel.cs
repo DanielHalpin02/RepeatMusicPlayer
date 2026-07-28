a977fef8c2fc15adf564409edc80739eb6d28e64
DanielHalpin02 <G00436639@atu.ie>
2026-07-28 19:11:07 +0100

    add LibraryViewModel

--- ViewModels/LibraryViewModel.cs
+++ ViewModels/LibraryViewModel.cs
@@ -1,12 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using RepeatMusicPlayer.Models;
+using RepeatMusicPlayer.Services;
 
-namespace RepeatMusicPlayer.ViewModels
+namespace RepeatMusicPlayer.ViewModels;
+
+public class LibraryViewModel : INotifyPropertyChanged
 {
-    internal class LibraryViewModel
+    private readonly LibraryService _libraryService = new();
+
+    public ObservableCollection<Song> Songs { get; set; } = new();
+
+    private Song _selectedSong;
+    public Song SelectedSong
     {
+        get => _selectedSong;
+        set
+        {
+            _selectedSong = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public LibraryViewModel()
+    {
+        var songs = _libraryService.GetSongs();
+        foreach (var song in songs)
+            Songs.Add(song);
+    }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+    protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
     }
 }
