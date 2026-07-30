10b49f52a4e0acfeb9a4cf698906b24a14a9ba24
DanielHalpin02 <G00436639@atu.ie>
2026-07-30 19:45:52 +0100

    started building out playlists

---
+++ Models/Playlist.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.ObjectModel;
+
+namespace RepeatMusicPlayer.Models;
+
+public class Playlist
+{
+    public string Name { get; set; }
+    public ObservableCollection<Song> Songs { get; set; } = new();
+}
---
+++ ViewModels/PlaylistViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using RepeatMusicPlayer.Models;
+
+namespace RepeatMusicPlayer.ViewModels;
+
+public class PlaylistViewModel : INotifyPropertyChanged
+{
+    public ObservableCollection<Playlist> Playlists { get; set; } = new();
+
+    private string _newPlaylistName;
+    public string NewPlaylistName
+    {
+        get => _newPlaylistName;
+        set
+        {
+            _newPlaylistName = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public void AddPlaylist()
+    {
+        if (string.IsNullOrWhiteSpace(NewPlaylistName))
+            return;
+
+        Playlists.Add(new Playlist { Name = NewPlaylistName });
+        NewPlaylistName = string.Empty;
+    }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+    protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
